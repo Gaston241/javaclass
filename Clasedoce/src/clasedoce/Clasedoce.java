706fc369499c5cc4b199b3bbf475316dc42df5f9
Gaston241 <gaston.500@hotmail.com>
2023-05-19 23:11:38 -0300

    clasedoce
    Ejercicio: Caracteres Especiales con Java

---
+++ Clasedoce/src/clasedoce/Clasedoce.java
@@ -0,0 +1,18 @@
+package clasedoce;
+
+// Clase numero 12 de Java! (Caracteres Especiales con Java)
+public class Clasedoce {
+
+    public static void main(String[] args) {
+
+        var nombre = "karla";
+
+        System.out.println("Nueva linea: \n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \b" + nombre);
+        System.out.println("Retroceso: \b\b" + nombre);
+        System.out.println("Comilla simple: \'" + nombre + "'");
+        System.out.println("Comilla doble: \"" + nombre + "\"");
+    }
+
+}
