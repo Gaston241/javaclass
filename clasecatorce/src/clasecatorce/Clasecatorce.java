d96fad0e1b5ff3a7407c14844d00059382c3d406
Gaston241 <gaston.500@hotmail.com>
2023-05-22 22:12:33 -0300

    clasecatorce
    Tarea usando Scanner!

---
+++ clasecatorce/src/clasecatorce/Clasecatorce.java
@@ -0,0 +1,24 @@
+package clasecatorce;
+
+//Clase numero 14 de Java! (Tarea utilizando Scanner.
+import java.util.Scanner;
+
+public class Clasecatorce {
+
+    public static void main(String[] args) {
+        Scanner consola = new Scanner (System.in);
+        System.out.println("Proporciona el Titulo:");
+        var Titulo = consola.nextLine();
+        System.out.println("Proporciona el Autor:");
+        var autor = consola.nextLine();
+        System.out.println("Titulo: " + Titulo);
+        System.out.println("Autor: " + autor);
+       
+                
+                
+        
+        
+        
+    }
+
+}
