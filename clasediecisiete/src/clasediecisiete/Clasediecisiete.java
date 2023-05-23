4b4efe164c0c7bc55373fe0b8d5bf74d6fb338db
Gaston241 <gaston.500@hotmail.com>
2023-05-22 22:36:06 -0300

    clasediecisiete
    Ejercicio: Tipos Primitivos tipos Flotantes

---
+++ clasediecisiete/src/clasediecisiete/Clasediecisiete.java
@@ -0,0 +1,23 @@
+package clasediecisiete;
+
+//Clase numero 17 de Java!(Tipos primitivos tipos Flotantes)
+public class Clasediecisiete {
+
+    public static void main(String[] args) {
+      /*
+        tipos primitivos de tipo flotante: float y double
+        */  
+
+     float numeroFloat = (float)3.4028236E38D;
+        System.out.println("numeroFloat = "+ numeroFloat);
+        System.out.println("Valor minimo tipo float: " + Float.MIN_VALUE);
+        System.out.println("Valor maximo tipo float: " + Float.MAX_VALUE);
+        
+        double numeroDouble = 1.7976931348623157E308;
+        System.out.println("numeroDouble" + numeroDouble);
+        System.out.println("Valor maximo double: " + Double.MAX_VALUE);
+        System.out.println("Valor minimo double: " + Double.MIN_VALUE);
+      
+    }
+
+}
