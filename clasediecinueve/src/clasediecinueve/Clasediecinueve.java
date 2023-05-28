aed56c30104b595bfca378a08d74013bc2a7e739
Gaston241 <gaston.500@hotmail.com>
2023-05-27 22:05:21 -0300

    clasediecinueve
    Ejercicio: Tipos Primitivos tipo char

---
+++ clasediecinueve/src/clasediecinueve/Clasediecinueve.java
@@ -0,0 +1,35 @@
+package clasediecinueve;
+//Clase numero 19 de Java! (Tipos primitivos tipo char)
+
+public class Clasediecinueve {
+    
+    public static void main(String[] args) {
+        char miCaracter = 'a';
+        System.out.println("miCaracter = " + miCaracter);
+        
+        char varChar = '\u0021';
+        System.out.println("varChar = " + varChar);
+        
+        char varCharDecimal = 33;
+        System.out.println("varCharDecimal = " + varCharDecimal);
+        
+        char varCharSimbolo = '!';
+        System.out.println("varCharSimbolo = " + varCharSimbolo);
+        
+        var varChar1 = '\u0021';
+        System.out.println("varChar1 = " + varChar1);
+        
+        var varCharDecimal2 = 33;
+        System.out.println("varCharDecimal2 = " + varCharDecimal2);
+        
+        var varCharSimbolo3 = '!';
+        System.out.println("varCharSimbolo3 = " + varCharSimbolo3);
+        
+        int variableEnteraASimbolo = '!';
+        System.out.println("variableEnteraASimbolo = " + variableEnteraASimbolo);
+        
+        int letra = 'A';
+        System.out.println("letra = " + letra);
+    }
+    
+}
